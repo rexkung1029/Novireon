e0f570dfe775d570aada23e37a7ec7ad4977a28a
rexkung1029 <rexkung1029@gmail.com>
2025-09-27 22:42:40 +0800

    forget something

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ FONT_PATH = os.path.join(PROJECT_ROOT, "data", "GenSenRounded2-M.ttc")
 DISCORD_DEFAULT_AVATAR = os.path.join(
     PROJECT_ROOT, "data", "default_discord_user_avatar.png"
 )
+DEFAULT_AVATAR = os.path.join(PROJECT_ROOT, "data", "default_avatar.jpg")
